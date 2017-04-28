3c89ac582570489dadf31380542724c89e7d854d
grishberg <grishberg@MBP-Grigory.lan>
2017-04-28 19:27:46 +0700

    rename packages

--- reststacklib/src/main/java/com/github/grigoryrylov/reststacklib/exception/BaseException.java
+++ reststacklib/src/main/java/com/github/grigoryrylov/reststacklib/exception/BaseException.java
@@ -2,12 +2,10 @@ package com.github.grigoryrylov.reststacklib.exception;
 
 /**
  * Created by grishberg on 23.04.17.
+ * base exception class
  */
 
 public class BaseException extends Exception {
-    public BaseException() {
-    }
-
     public BaseException(String message) {
         super(message);
     }
